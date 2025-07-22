e4020ab34af69d7c5d200209b408080c20254833
ShivamKGate <sinayksp@mail.uc.edu>
2025-07-21 23:08:05 -0400

    updated frontends and backend starters

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Smart-IMS API is running."}
