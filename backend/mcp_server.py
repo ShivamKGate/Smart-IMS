631b568da327abdff688218dad6c1d5e313bf123
ShivamKGate <sinayksp@mail.uc.edu>
2025-07-30 22:27:32 -0400

    db connection to mcp server

--- backend/mcp_server.py
+++ backend/mcp_server.py
@@ -1,11 +1,29 @@
+import os
+import psycopg2
 from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+load_dotenv()
 
 mcp = FastMCP("Smart-IMS DB Server")
 
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+
 @mcp.tool()
 def execute_sql_query(sql: str):
-    # stub
-    return [{"status": "not implemented"}]
-
-if __name__ == "__main__":
-    mcp.run()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(sql)
+    try:
+        results = cur.fetchall()
+    except:
+        conn.commit()
+        results = []
+    conn.close()
+    return results
