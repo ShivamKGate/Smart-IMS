96c1a60ea2f082946e024006e96dc05af71ef05d
ShivamKGate <sinayksp@mail.uc.edu>
2025-07-30 22:33:45 -0400

    integrated mcp endpoint and added pydantic model with simple error handling

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+from mcp_client import MCPClient
+from pydantic import BaseModel
 
 load_dotenv()
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
 
-@app.get("/")
-def read_root():
-    return {"message": "Smart-IMS API is running."}
+class QueryRequest(BaseModel):
+    question: str
+
+@app.post("/api/query")
+async def natural_language_query(request: QueryRequest):
+    sql_result = await MCPClient.call_tool("text_to_sql", {"text": request.question})
+    if not sql_result["success"]:
+        raise HTTPException(status_code=500)
+    return sql_result["result"]
